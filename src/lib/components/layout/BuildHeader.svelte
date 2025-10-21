9d53cc78623a6da22fdc0859648f44e8fb74ba15
Fadi Hareth <fadihareth@gmail.com>
2025-10-21 12:29:16 -0400

    cleanup buildheader ui

--- src/lib/components/layout/BuildHeader.svelte
+++ src/lib/components/layout/BuildHeader.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { charactersById } from '$lib/stores/data';
 	import { buildsState } from '$lib/stores/state.svelte';
-    import { IconCircleArrowLeftFilled } from '@tabler/icons-svelte';
+	import { IconCircleArrowLeftFilled } from '@tabler/icons-svelte';
 
 	let {
 		id = $bindable(),
@@ -14,25 +14,32 @@
 	);
 </script>
 
-<header class="sticky top-0 z-50 m-4 flex items-center justify-between rounded-full bg-genshin-blue/90 shadow-md backdrop-blur">
-	<div class="flex gap-10 py-4 px-10">
+<header
+	class="sticky top-0 z-50 m-4 flex items-center justify-between rounded-full bg-genshin-blue/90 shadow-md backdrop-blur"
+>
+	<div class="flex gap-6 px-10 py-2">
 		<button onclick={reset}>
-            <IconCircleArrowLeftFilled size={42} class="fill-genshin-white" />
-        </button>
-		<div class="hidescrollbar flex gap-10 overflow-x-scroll">
+			<IconCircleArrowLeftFilled size={36} class="fill-genshin-white" />
+		</button>
+		<div class="hidescrollbar flex gap-6 overflow-x-scroll">
 			{#each orderedBuilds as build (build.id)}
-				<button
-					onclick={() => {
-						update(build.id);
-					}}
-                    style={`view-transition-name: build-${build.id};`}
-				>
-					<img
-						src={charactersById[build.character].iconImage}
-						alt="icon"
-						class={`w-15 h-15 rounded-full border-genshin-gold ${id == build.id && 'border-2'}`}
-					/>
-				</button>
+                <div class="flex flex-col items-center gap-2">
+                    <button
+                        onclick={() => {
+                            update(build.id);
+                        }}
+                        style={`view-transition-name: build-${build.id};`}
+                    >
+                        <img
+                            src={charactersById[build.character].iconImage}
+                            alt="icon"
+                            class={`h-11 w-11 rounded-full border-2 ${id === build.id ? 'border-genshin-gold' : 'border-gray-500'}`}
+                        />
+                    </button>
+                    {#if id === build.id}
+                        <div class="h-0 w-6 border-1 border-genshin-gold"></div>
+                    {/if}
+                </div>
 			{/each}
 		</div>
 	</div>
