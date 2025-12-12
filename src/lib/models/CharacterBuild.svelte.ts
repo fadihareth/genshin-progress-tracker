e47687573dab0b85fb09afe0736d9bb48675a683
Fadi Hareth <fadihareth@gmail.com>
2025-12-12 15:44:42 -0500

    add artifact fields to characterbuild

--- src/lib/models/CharacterBuild.svelte.ts
+++ src/lib/models/CharacterBuild.svelte.ts
@@ -16,6 +16,18 @@ export class CharacterBuild {
     currWeaponRefine: number = $state(0);
     targetWeaponRefine: number = $state(5);
 
+    // Artifact Progress
+    artifactIds: number[] = [];
+    flowerStatus: boolean = false;
+    plumeStatus: boolean = false;
+    sandsStat: string | null = null;
+    sandsStatus: boolean = false;
+    gobletStat: string | null = null;
+    gobletStatus: boolean = false;
+    circletStat: string | null = null;
+    circletStatus: boolean = false;
+    artifactSubstats: string[] = [];
+
     // Talents Progress
     currTalent1Level: number = $state(1);
     targetTalent1Level: number = $state(10);
@@ -28,18 +40,32 @@ export class CharacterBuild {
         this.id = id;
         this.character = character;
         this.order = id;
-        this.currLevel = buildValues.currLevel;
-        this.targetLevel = buildValues.targetLevel;
-        this.weaponId = buildValues.weaponId;
-        this.currWeaponLevel = buildValues.currWeaponLevel;
-        this.targetWeaponLevel = buildValues.targetWeaponLevel;
-        this.currWeaponRefine = buildValues.currWeaponRefine;
-        this.targetWeaponRefine = buildValues.targetWeaponRefine;
-        this.currTalent1Level = buildValues.currTalent1Level;
-        this.targetTalent1Level = buildValues.targetTalent1Level;
-        this.currTalent2Level = buildValues.currTalent2Level;
-        this.targetTalent2Level = buildValues.targetTalent2Level;
-        this.currTalent3Level = buildValues.currTalent3Level;
-        this.targetTalent3Level = buildValues.targetTalent3Level;
+
+        this.currLevel = buildValues.currLevel ?? 1;
+        this.targetLevel = buildValues.targetLevel ?? 90;
+
+        this.weaponId = buildValues.weaponId ?? null;
+        this.currWeaponLevel = buildValues.currWeaponLevel ?? 1;
+        this.targetWeaponLevel = buildValues.targetWeaponLevel ?? 90;
+        this.currWeaponRefine = buildValues.currWeaponRefine ?? 0;
+        this.targetWeaponRefine = buildValues.targetWeaponRefine ?? 5;
+
+        this.artifactIds = buildValues.artifactIds ?? [];
+        this.flowerStatus = buildValues.flowerStatus ?? false;
+        this.plumeStatus = buildValues.plumeStatus ?? false;
+        this.sandsStat = buildValues.sandsStat ?? null;
+        this.sandsStatus = buildValues.sandsStatus ?? false;
+        this.gobletStat = buildValues.gobletStat ?? null;
+        this.gobletStatus = buildValues.gobletStatus ?? false;
+        this.circletStat = buildValues.circletStat ?? null;
+        this.circletStatus = buildValues.circletStatus ?? false;
+        this.artifactSubstats = buildValues.artifactSubstats ?? [];
+
+        this.currTalent1Level = buildValues.currTalent1Level ?? 1;
+        this.targetTalent1Level = buildValues.targetTalent1Level ?? 10;
+        this.currTalent2Level = buildValues.currTalent2Level ?? 1;
+        this.targetTalent2Level = buildValues.targetTalent2Level ?? 10;
+        this.currTalent3Level = buildValues.currTalent3Level ?? 1;
+        this.targetTalent3Level = buildValues.targetTalent3Level ?? 10;
     }
 }
