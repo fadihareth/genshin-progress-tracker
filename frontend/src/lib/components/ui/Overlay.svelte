59f1d77381742d3ef6120196d896a77decf55a66
Fadi Hareth <fadihareth@gmail.com>
2026-03-19 13:46:57 -0400

    fix overlay tag

--- frontend/src/lib/components/ui/Overlay.svelte
+++ frontend/src/lib/components/ui/Overlay.svelte
@@ -3,10 +3,11 @@
 </script>
 
 {#if open}
-	<button
+    <!-- svelte-ignore a11y_click_events_have_key_events, a11y_no_static_element_interactions -->
+	<div
         class="fixed inset-0 z-100 flex items-center justify-center bg-black/50"
         onclick={(e) => e.target === e.currentTarget && onClose()}
     >
 		{@render children?.()}
-	</button>
+	</div>
 {/if}
